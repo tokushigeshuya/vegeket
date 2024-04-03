ed70f759174691d67381c1e26696af6ccf31e142
tokushigeshuya <narihare1130@gmail.com>
2024-04-03 23:48:15 +0900

    [add]classの設定

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from base.models import Item,Category,Tag
+from django.contrib.auth.models import Group
 
-# Register your models here.
+class TagInliine(admin.TabularInline):
+    model = Item.tags.through
+
+class ItemAdmin(admin.ModelAdmin):
+    inlines = [TagInliine]
+    # 除外する項目
+    exclude = ['tags']
+
+# 表示
+admin.site.register(Item,ItemAdmin)
+admin.site.register(Category)
+admin.site.register(Tag)
+# 非表示
+admin.site.unregister(Group)
